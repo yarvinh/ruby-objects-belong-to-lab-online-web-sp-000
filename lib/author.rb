a3b351a566d829908bfeb6f09358a09fe08fd2ee
binding-archive-7591 <yarhumher@hotmail.com>
2020-03-24 00:46:33 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,7 @@
+class Author
+  attr_accessor :name, :author
+  # def initialize
+  #   @name
+  #   @autor
+  # end
+end
---
+++ lib/post.rb
@@ -0,0 +1,6 @@
+class Post
+  attr_accessor :title, :author
+  def initialize
+    @title
+  end
+end
